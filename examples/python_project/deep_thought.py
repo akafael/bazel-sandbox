5e01117cbf59ab6ab4764144cec56a781b97e92b
Rafael <akafael@users.noreply.github.com>
2025-07-01 23:12:49 -0300

    Add an example with function

---
+++ examples/python_project/deep_thought.py
@@ -0,0 +1,8 @@
+"""Deep Thought."""
+
+def deep_thought(prompt:str =""):
+    """Runs the most powerfull AI system ever build."""
+    return 42
+
+if __name__ == "__main__":
+    print(deep_thought())
